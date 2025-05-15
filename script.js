9a817564d99f1d0ecfa02bdbac94a02b0eca8cfb
Rustam <189756814+rustleium@users.noreply.github.com>
2025-05-15 10:41:44 +0530

    feat(Gameboard): add function to reset board

--- script.js
+++ script.js
@@ -13,9 +13,18 @@ const Gameboard = (function () {
     board[row][column] = marker;
   }
 
+  function resetBoard() {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        board[i][j] = ""
+      }
+    }
+  }
+
   return {
     showBoard,
     placeMark,
+    resetBoard,
   }
 })();
 
